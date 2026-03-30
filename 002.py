6cdaddd7160390dd1713386d19e338a3148b1590
9nency-debug <9.nency@gmail.com>
2026-03-31 00:02:40 +0530

    Added

---
+++ 002.py
@@ -0,0 +1,13 @@
+num1 = int(input("\n"))
+num2 = int(input("\n"))
+num3 = int(input("\n"))
+num4 = int(input("\n"))
+num5 = int(input("\n"))
+num6 = int(input("\n"))
+num7 = int(input("\n"))
+num8 = int(input("\n"))
+
+s = {num1, num2, num3, num4, num5, num6, num7, num8}
+print(s)
+
+# /Users/Pranshiv/Nenshiv/Harry_Bhaiya_Python/Dic/002.py
